2595cd53272811c47605d87923ccc27cc01d5224
Michael Salvati <print@3daddict.com>
2018-09-14 09:26:02 -0700

    React-Intro - MIchael Salvati

--- src/index.js
+++ src/index.js
@@ -1,7 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const USER = {
+    name: "Michael Salvati",
+    luckyNumber: LuckyNumber(),
+    }
+
+function LuckyNumber() {
+    let luckyNumber = 0;
+    luckyNumber = Math.floor((Math.random() * 1000) + 1);
+    return luckyNumber;
+}
+
+function Greeting(props) {
+    return (
+        <div className="container">
+            <h1>{props.name}</h1>
+            <h2 className="text-muted">This is your lucky number: {props.luckyNumber}</h2>
+        </div>
+    );
+}
+
+const user = {
+    firstName: 'Mike',
+    lastName: 'Salvati'
+};
+
+function fullName(user){
+    return `${user.firstName} ${user.lastName}`;
+}
+
+const greeting = <h1>Hello {fullName(user)}</h1>;
+
 ReactDOM.render(
-    <h1 className="container">React Prototypes!</h1>,
+    // <App />,
+    Greeting(USER),
     document.getElementById('root')
 );
