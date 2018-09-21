e5cc678790528ae3d14a6a268b0988156dae4940
Michael Salvati <print@3daddict.com>
2018-09-20 17:27:27 -0700

    styles image scrapbook completed

--- src/components/scrapbook.js
+++ src/components/scrapbook.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import './scrapbook.css';
 import ImageData from './image_data'
-import ScrapbookImages from './scrapbook_images'
+import ScrapbookImage from './scrapbook_images'
 
 export default () => {
+    const images = ImageData.map((img, index) => {
+        return <ScrapbookImage key={index} about={img} />
+    });
 
     return (
         <div className="scrapbook-container">
-            <ScrapbookImages about={ImageData[0]}/>
+            {images}
         </div>
     )
 }
--- src/components/scrapbook_images.js
+++ src/components/scrapbook_images.js
@@ -3,6 +3,7 @@ import React from 'react';
 export default props => {
     console.log('props = ', props);
     const {src} = props.about;
+    const {style} = props.about;
 
-    return <img src={src} alt=""/>
+    return <img className="scrapbook-image" src={src} style={style} />
 }
