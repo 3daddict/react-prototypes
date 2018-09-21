992c113b695c540218a467b873a5b4518f62f418
Michael Salvati <print@3daddict.com>
2018-09-21 14:24:52 -0700

    working on react axios

---
+++ src/components/movie_container.js
@@ -0,0 +1,13 @@
+import {Component} from 'react';
+import Movie from './movie'
+
+class MoviesContainer extends Component {
+    render() {
+        <div className="container">
+            <h2>Movie Container</h2>
+            <h2><Movie /></h2>
+        </div>
+    }
+}
+
+//feature set 1 last set of instructions
