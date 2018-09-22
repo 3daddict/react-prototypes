17e537a0472b3277edc850e2d026d86c85304be1
Michael Salvati <print@3daddict.com>
2018-09-22 09:56:14 -0700

    app.js an welcome.js added

--- src/components/app.js
+++ src/components/app.js
@@ -1,2 +1,10 @@
+import React from 'react';
+import {Route} from 'react-router-dom';
+import Welcome from './welcome';
 
-
+export default () => (
+    <div className="container">
+        <h1>Heading 1 here!</h1>
+        <Route path="/" component={Welcome} />
+    </div>
+)
